1dbe809b1868713f11cb6e08627d5daba109f88b
Joply <joply3006@gmail.com>
2026-06-18 22:01:54 +0200

    Backfill: echte GPPoule data alle 7 races (Bahrein t/m Barcelona)

--- data.js
+++ data.js
@@ -2,18 +2,18 @@
 // Het bevat de poule stand per race, zodat de website altijd actueel is.
 
 const POULE_DATA = {
-  races: ["Bahrein", "Saudi-Arabie", "Australie", "Japan", "China", "Monaco", "Barcelona"],
+  races: ["Bahrein","Saudi-Arabie","Australie","Japan","China","Monaco","Barcelona"],
   spelers: {
-    "Jessiebommele5275": [8, 15, 22, 38, 52, 68, 91],
-    "Dennisbommele":     [10, 18, 24, 33, 49, 65, 88],
-    "FrankKalkman4423":  [6, 14, 26, 40, 55, 70, 87],
-    "SannieB27":         [9, 16, 20, 35, 48, 72, 87],
-    "pkalkman":          [4, 12, 19, 28, 44, 62, 82],
-    "EenGamendeVisYT1269": [5, 10, 15, 22, 35, 50, 81],
-    "Christina":         [3, 9, 18, 26, 40, 55, 73],
-    "Dinnekops":         [2, 8, 14, 20, 33, 48, 69],
-    "B@rt":              [4, 9, 13, 19, 29, 45, 67],
-    "Verslappen":        [3, 7, 11, 18, 28, 42, 60],
-    "DanielaPolak3440":  [1, 4, 8, 14, 22, 33, 47]
+    "Jessiebommele5275": [41, 56, 60, 81, 87, 91, 97],
+    "SannieB27": [44, 63, 69, 80, 84, 87, 95],
+    "Dennisbommele": [50, 63, 69, 84, 85, 88, 94],
+    "pkalkman": [42, 54, 59, 76, 77, 82, 90],
+    "FrankKalkman4423": [41, 56, 62, 79, 84, 87, 90],
+    "EenGamendeVisYT1269": [40, 51, 64, 72, 77, 81, 84],
+    "Christina": [36, 54, 57, 65, 68, 73, 80],
+    "Dinnekops": [36, 58, 60, 68, 68, 69, 79],
+    "B@rt": [35, 42, 50, 57, 64, 67, 72],
+    "Verslappen": [27, 37, 46, 59, 60, 60, 68],
+    "DanielaPolak3440": [27, 30, 31, 38, 43, 47, 54]
   }
 };
